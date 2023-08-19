32adb5950c165f65973e1bb9963ecbc08d7b489a
mich1991 <wozniak.michal91@gmail.com>
2023-08-19 18:41:07 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {NavigationComponent} from "./components/navigation/navigation.component";
+import {HomeComponent} from "./components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : '', component: HomeComponent, pathMatch:'full'},
+  {path : 'stores', component: HomeComponent, pathMatch:'full'},
+  {path : 'movies', component: HomeComponent, pathMatch:'full'},
+  {path : 'actors', component: HomeComponent, pathMatch:'full'},
+  {path : 'admin-staff', component: HomeComponent, pathMatch:'full'},
+];
